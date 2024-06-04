c1161653125e76f9d5b465d3749fa2ad70b1c5ca
IzanamiiDevv <rafaeloli596@gmail.com>
2024-06-04 18:53:29 +0800

    C++ Code

---
+++ lambda.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+void forEach(std::string txt, void(*func)(char));
+void forEach(std::vector<int> arr, void(*func)(int));
+void forEach(std::vector<std::string> arr, void(*func)(std::string));
+
+int main() {
+    
+    std::vector<int> numbers = {10,39,21,38};
+    std::vector<std::string> names = {"Gab", "Rose", "Kyle"};
+    
+    forEach("Hello", [](char c) -> void {
+        std::cout << c << std::endl;
+    });
+    
+    forEach(numbers, [](int x) -> void {
+        int z = x + 10;
+        std::cout << z << std::endl;
+    });
+    
+    forEach(names, [](std::string name) -> void {
+        std::cout << name << std::endl;
+    });
+
+    return 0;
+}
+
+//string
+void forEach(std::string txt, void(*func)(char)) {
+    for(int i = 0; i < txt.length(); i++) {
+        func(txt[i]);
+    }
+}
+
+//vector int
+void forEach(std::vector<int> arr, void(*func)(int)) {
+    for(int i = 0; i < arr.size(); i++) {
+        func(arr[i]);
+    }
+}
+
+//vector string
+void forEach(std::vector<std::string> arr, void(*func)(std::string)) {
+    for(int i = 0; i < arr.size(); i++) {
+        func(arr[i]);
+    }
+}
